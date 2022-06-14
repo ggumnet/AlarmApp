6cf325cf8d1931393f04a64b8a74cf542791aab7
ggumnet <ggumnet@snu.ac.kr>
2022-06-14 13:02:54 +0900

    first

---
+++ app/src/main/java/com/example/alarmapp/AlarmAnalysisActivity.java
@@ -0,0 +1,16 @@
+package com.example.alarmapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+//알람 분석하기
+public class AlarmAnalysisActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_analysis);
+    }
+}
---
+++ app/src/main/java/com/example/alarmapp/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.alarmapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.UUID;
+
+
+
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] permission_list = {
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.BLUETOOTH,
+                Manifest.permission.BLUETOOTH_SCAN,
+                Manifest.permission.BLUETOOTH_ADVERTISE,
+                Manifest.permission.BLUETOOTH_CONNECT,
+        };
+        ActivityCompat.requestPermissions(MainActivity.this, permission_list, 1);
+
+    }
+
+    public void runAlarmSet(View view){
+        Intent intent = new Intent(this, AlarmSetActivity.class);
+        startActivity(intent);
+    }
+    public void runAlarmAnalysis(View view){
+        Intent intent = new Intent(this, AlarmAnalysisActivity.class);
+        startActivity(intent);
+    }
+    public void runBluetoothSet(View view){
+        Intent intent = new Intent(this, BluetoothSet.class);
+        startActivity(intent);
+    }
+}
